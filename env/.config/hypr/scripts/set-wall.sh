c42098b747f0b0513d7ec33f351fe58ea1a4723a
Nian0bussou <nianlegare@outlook.com>
2025-05-31 18:46:58 -0400

    added randomizer to set-wall.sh

--- env/.config/hypr/scripts/set-wall.sh
+++ env/.config/hypr/scripts/set-wall.sh
@@ -1,54 +1,79 @@
 #!/usr/bin/bash
 
-wall=$(swww query | head -n 1 | sed -n 's|.*/\([^/.]*\)\..*|\1|p')
-if [ -z $current_wall ]
-then
-    current_wall=$(ps aux | grep mpvpaper | head -n 1 | sed -n 's|.*/\([^/.]*\)\..*|\1|p')
-fi
-
 
 directory="$HOME/personal/wallpapers"
-tmpfile=$(mktemp)
 
-# Gather files, ignoring .git
-mapfile -t files < <(find "$directory" -path "$directory/.git" -prune -o -type f -print | sort)
+set_wall() {
+    full_path="$1"
+    pkill mpvpaper
+    pkill swww
 
-options=()
-for full_path in "${files[@]}"; do
-    filename=$(basename "$full_path")
-    options+=("$filename")
-    echo "$filename|$full_path" >> "$tmpfile"
-done
+    extension="${full_path##*.}"
+    case "$extension" in
+        jpg|jpeg|png|bmp|webp)
+            # Use swww
+            swww-daemon & disown
+            sleep 1
+            swww img "$full_path" --transition-step 100 --transition-fps 144
+            notify-send "Wallpaper" "Switching to $full_path"
+            ;;
+        mp4|webm|mkv|mov)
+            mpvpaper -p -o "--panscan=1.0 vf-add=fps=10:round=near no-audio --loop-file=inf" ALL "$full_path" &
+            notify-send "Wallpaper" "Switching to $full_path"
+            ;;
+        *)
+            notify-send "Unsupported file type: $extension"
+            ;;
+    esac
+}
 
-# Prompt user to select
-selected=$(printf '%s\n' "${options[@]}" | rofi -dmenu -i -p "Current : $current_wall" -matching glob -theme ~/.config/rofi/walls.rasi)
+randomized() {
+    wall=$(find  "$directory" -type f | shuf -n 1)
 
-if [ -n "$selected" ]; then
-    full_path=$(grep "^$selected|" "$tmpfile" | cut -d'|' -f2-)
+    set_wall "$wall"
+}
 
-    if [ -n "$full_path" ]; then
-        # Kill existing mpvpaper/hyprpaper processes
-        pkill mpvpaper
-        pkill swww
-
-        extension="${full_path##*.}"
-        case "$extension" in
-            jpg|jpeg|png|bmp|webp)
-                # Use swww
-                swww-daemon & disown
-                sleep 1
-                swww img "$full_path" --transition-step 100 --transition-fps 144
-                notify-send "Wallpaper" "Switching to $full_path"
-                ;;
-            mp4|webm|mkv|mov)
-                mpvpaper -p -o "--panscan=1.0 vf-add=fps=10:round=near no-audio --loop-file=inf" ALL "$full_path" &
-                notify-send "Wallpaper" "Switching to $full_path"
-                ;;
-            *)
-                notify-send "Unsupported file type: $extension"
-                ;;
-        esac
+selected() {
+    wall=$(swww query | head -n 1 | sed -n 's|.*/\([^/.]*\)\..*|\1|p')
+    if [ -z $current_wall ]
+    then
+        current_wall=$(ps aux | grep mpvpaper | head -n 1 | sed -n 's|.*/\([^/.]*\)\..*|\1|p')
     fi
-fi
 
-rm -f "$tmpfile"
+    tmpfile=$(mktemp)
+
+    # Gather files, ignoring .git
+    mapfile -t files < <(find "$directory" -path "$directory/.git" -prune -o -type f -print | sort)
+
+    options=()
+    for full_path in "${files[@]}"; do
+        filename=$(basename "$full_path")
+        options+=("$filename")
+        echo "$filename|$full_path" >> "$tmpfile"
+    done
+
+    # Prompt user to select
+    selected=$(printf '%s\n' "${options[@]}" | rofi -dmenu -i -p "Current : $current_wall" -matching glob -theme ~/.config/rofi/walls.rasi)
+
+    if [ -n "$selected" ]; then
+        full_path=$(grep "^$selected|" "$tmpfile" | cut -d'|' -f2-)
+
+        if [ -n "$full_path" ]; then
+            set_wall "$full_path"
+        fi
+    fi
+
+    rm -f "$tmpfile"
+}
+
+case $1 in
+    random)
+        randomized
+        ;;
+    select)
+        selected
+        ;;
+    *)
+        echo "No valid option selected"
+        ;;
+esac
