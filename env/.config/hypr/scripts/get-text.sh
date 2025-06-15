c0121bd08896abed4d5bce4c70998d59d00a334a
laeva-lady <alicelegare@outlook.com>
2025-06-14 21:52:20 -0400

    chore: automated commit

--- env/.config/hypr/scripts/get-text.sh
+++ env/.config/hypr/scripts/get-text.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/bash
 
 
+lang_choices="eng
+jpn"
+
+lang_selected=$(echo "$lang_choices" | rofi -dmenu -p "Language" -i -theme ~/.config/rofi/config.rasi)
+
 # Take a screenshot of selected region and OCR it
 tmpfile=$(mktemp /tmp/ocr_XXXXXX.png)
 grim -g "$(slurp)" "$tmpfile"
-tesseract "$tmpfile" - -l eng | wl-copy
-notify-send "OCR" "Text copied to clipboard"
+if tesseract "$tmpfile" - -l "$lang_selected" | wl-copy; then
+  notify-send "OCR" "Text copied to clipboard"
+else
+  notify-send "OCR Error" "Failed to process or copy text"
+fi
 rm "$tmpfile"
+
+# 神の
