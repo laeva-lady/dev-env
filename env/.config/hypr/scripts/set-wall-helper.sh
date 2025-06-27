f2372d267818320a38005eac1ce39803b454735e
laeva-lady <alicelegare@outlook.com>
2025-06-26 20:57:12 -0400

    chore: automated commit

--- env/.config/hypr/scripts/set-wall-helper.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-fzf \
-    --preview="grep {} "'"$filename"'""
-        | cut -d\"|\" -f2- \
-        | xargs chafa --size=70x" \
-    --preview-window=right:70%:wrap \
-    < "'"$tmp_options"'" > "'"$choice_tmp"'"
