11f1272160303d9531077f785b65ca2a4dacfa0a
laeva-lady <alicelegare@outlook.com>
2025-06-30 17:25:24 -0400

    chore: automated commit

--- env/.config/hypr/scripts/web-app.sh
+++ env/.config/hypr/scripts/web-app.sh
@@ -3,12 +3,12 @@ options="start.duckduckgo.com
 reddit.com
 comick.io
 twitter.com
-youtube.com"
+youtube.com
+chatgpt.com"
 selection=$(echo "${options[@]}" | rofi -dmenu -theme ~/.config/rofi/aorstie.rasi)
 
 if [ -n "$selection" ]
 then
-    zen-browser --new-tab "$selection"
-    hyprctl dispatch workspace 1
+    firefox --new-window "$selection" &
 fi
 
