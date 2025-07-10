f9a0b73daf4a5d668584b4b159000042d7328f7a
laeva-lady <alicelegare@outlook.com>
2025-07-10 14:04:43 -0400

    chore: automated commit

---
+++ dotfiles.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+confs=(
+    dunst
+    fastfetch
+    fcitx
+    fcitx5
+    ghostty
+    hyfetch.json
+    hypr
+    i3
+    kickoff
+    kitty
+    nvim
+    ranger
+    rofi
+    waybar
+    zed
+)
+
+envs=(
+    ".aliases.sh"
+    ".bashrc"
+    ".zshrc"
+    ".tmux.conf"
+    ".Xresources"
+)
+
