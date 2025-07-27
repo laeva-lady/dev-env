cf4f516429f4ec437791eb3a3ede86553d1d2fdf
laeva-lady <alicelegare@outlook.com>
2025-07-26 20:36:23 -0400

    chore: automated commit

--- env/.config/hypr/scripts/toggle-bar.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-if pgrep waybar > /dev/null
-then
-    pkill waybar
-else
-    ~/.config/waybar/waybar.sh &
-fi
--- env/.config/waybar/waybar.sh
+++ env/.config/waybar/waybar.sh
@@ -1,10 +1,37 @@
+#!/usr/bin/bash
+
 style="kitter"
 
-if [ -d "$HOME/.config/waybar/$style" ]; then
-    WAYBAR_STYLE="$HOME/.config/waybar/$style"
-else
-    echo "Config Directory does not exists"
-    exit 1
-fi
+spawn_regular_bar() {
+    if [ -d "$HOME/.config/waybar/$style" ]; then
+        WAYBAR_STYLE="$HOME/.config/waybar/$style"
+    else
+        echo "Config Directory does not exist"
+        exit 1
+    fi
 
-waybar -c "$WAYBAR_STYLE/config.jsonc" -s "$WAYBAR_STYLE/style.css"
+    waybar -c "$WAYBAR_STYLE/config.jsonc" -s "$WAYBAR_STYLE/style.css" &
+}
+
+spawn_workspaces() {
+    waybar -c "$HOME/.config/waybar/work-only/config.jsonc" -s "$HOME/.config/waybar/work-only/style.css" &
+}
+
+case "$1" in
+"toggle")
+    REGULAR_CONFIG="$HOME/.config/waybar/$style/config.jsonc"
+    pid=$(pgrep -a waybar | grep "$REGULAR_CONFIG" | awk '{print $1}')
+
+    if [ -n "$pid" ]; then
+        echo "Killing regular bar (PID $pid)"
+        kill "$pid"
+    else
+        echo "Spawning regular bar"
+        spawn_regular_bar
+    fi
+    ;;
+*)
+    spawn_regular_bar
+    spawn_workspaces
+    ;;
+esac
