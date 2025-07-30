21a5326f756de7a8255fd89d235cb07c3bb59cce
laeva-lady <alicelegare@outlook.com>
2025-07-30 10:51:35 -0400

    chore: automated commit

--- env/.config/waybar/waybar.sh
+++ env/.config/waybar/waybar.sh
@@ -3,14 +3,7 @@
 style="kitter"
 
 spawn_regular_bar() {
-    if [ -d "$HOME/.config/waybar/$style" ]; then
-        WAYBAR_STYLE="$HOME/.config/waybar/$style"
-    else
-        echo "Config Directory does not exist"
-        exit 1
-    fi
-
-    waybar -c "$WAYBAR_STYLE/config.jsonc" -s "$WAYBAR_STYLE/style.css" &
+    waybar &
 }
 
 spawn_workspaces() {
@@ -19,29 +12,15 @@ spawn_workspaces() {
 
 case "$1" in
 "regular")
-    REGULAR_CONFIG="$HOME/.config/waybar/$style/config.jsonc"
-    pid=$(pgrep -a waybar | grep "$REGULAR_CONFIG" | awk '{print $1}')
-    if [ -n "$pid" ]; then
-        echo "Killing regular bar (PID $pid)"
-        kill "$pid"
-    else
-        echo "Spawning regular bar"
-        spawn_regular_bar
-    fi
+    pkill -x waybar
+    spawn_regular_bar
     ;;
 "work")
-    WORK_CONF="$HOME/.config/waybar/work-only/config.jsonc"
-    pid=$(pgrep -a waybar | grep "$WORK_CONF" | awk '{print $1}')
-    if [ -n "$pid" ]; then
-        echo "Killing work bar (PID $pid)"
-        kill "$pid"
-    else
-        echo "Spawning work bar"
-        spawn_workspaces
-    fi
+    pkill -x waybar
+    spawn_workspaces
     ;;
 *)
-    pkill waybar
+    pkill -x waybar
     spawn_regular_bar
     spawn_workspaces
     ;;
